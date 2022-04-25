575b7b1b39aa5a7c171ba2deaadebbe6835e7494
Vince Hartman <97495456+vhartman11@users.noreply.github.com>
2022-04-25 09:53:16 -0400

    Add files via upload
    put images into file
    created css file
    cleaned up code

--- pacman.js
+++ pacman.js
@@ -1,40 +1,36 @@
 let pos = 0;
-const pacArray = [
-  ['./PacMan1.png', './PacMan2.png'],
-  ['./PacMan3.png', './PacMan4.png'],
-];
-let direction = 0;
-const pacMen = []; // This array holds all the pacmen
 
-// This function returns an object with random values
-function setToRandom(scale) {
+const pacArray = [
+  './pacimgs/PacMan1.png',
+  './pacimgs/PacMan2.png',
+  './pacimgs/PacMan3.png',
+  './pacimgs/PacMan4.png',
+];
+
+let direction = 0;
+
+const pacMen = [];
+
+function setToRandom(scale) { //returns an object with random values
   return {
     x: Math.random() * scale,
     y: Math.random() * scale,
   };
 }
 
-// Factory to make a PacMan at a random position with random velocity
-function makePac() {
-  // returns an object with random values scaled {x: 33, y: 21}
-  let velocity = setToRandom(10); // {x:?, y:?}
+function makePac() { //makes pacman with random velocity and posistion
+  let velocity = setToRandom(10);
   let position = setToRandom(200);
-
-  // Add image to div id = game
   let game = document.getElementById('game');
   let newimg = document.createElement('img');
   newimg.style.position = 'absolute';
-  newimg.src = './PacMan1.png';
+  newimg.src = './pacimgs/PacMan1.png';
   newimg.width = 100;
-// TODO: set position here
   newimg.style.top = position.y
   newimg.style.left = position.x
-  
-  // TODO add new Child image to game
   game.appendChild(newimg);
 
-  // return details in an object
-  return {
+  return { //return object with random values
     position,
     velocity,
     newimg,
@@ -42,7 +38,7 @@ function makePac() {
 }
 
 function update() {
-  // loop over pacmen array and move each one and move image in DOM
+  // loop over pacArray and move each one and move image in DOM
   pacMen.forEach((item) => {
     checkCollisions(item);
     item.position.x += item.velocity.x;
@@ -54,11 +50,7 @@ function update() {
   setTimeout(update, 20);
 }
 
-
-
-
-function checkCollisions(item) {
-  // TODO: detect collision with all walls and make pacman bounce
+function checkCollisions(item) { //makes pacman bounce off the walls
   if (
     item.velocity.x + item.position.x + item.newimg.width > window.innerWidth ||
     item.velocity.x + item.position.x < 0
@@ -72,13 +64,6 @@ function checkCollisions(item) {
     item.velocity.y = -item.velocity.y;
 }
 
-
-
-
-
-
-
-
-function makeOne() {
-  pacMen.push(makePac()); // add a new PacMan
+function makeOne() { //adds new pacman
+  pacMen.push(makePac());
 }
